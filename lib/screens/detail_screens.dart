05a2e7298d4bf46f2c8bbcf474197e56ece4538a
leejungmin05 <74166576+leejungmin05@users.noreply.github.com>
2023-10-06 00:00:19 +0900

    add episode

--- lib/screens/detail_screens.dart
+++ lib/screens/detail_screens.dart
@@ -21,6 +21,7 @@ class _DetailScreenState extends State<DetailScreen> {
   late Future<WebtoonDetailModel> webtoon;
   late Future<List<WebtoonEpisodeModel>> episodes;
 
+  // home screen에서 API는 어떠한 인자도 필요없는데, 여기서는 Webtoon의 id가 필요하므로 initstate가 필요
   @override
   void initState() {
     super.initState();
@@ -44,60 +45,105 @@ class _DetailScreenState extends State<DetailScreen> {
           ),
         ),
       ),
-      body: Column(children: [
-        const SizedBox(
-          height: 50,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Hero(
-              tag: widget.id,
-              child: Container(
-                width: 250,
-                clipBehavior: Clip.hardEdge,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 15,
-                        offset: const Offset(10, 10),
-                        color: Colors.black.withOpacity(0.5),
-                      )
-                    ]),
-                child: Image.network(widget.thumb),
-              ),
-            ),
-          ],
-        ),
-        const SizedBox(height: 25),
-        FutureBuilder(
-            future: webtoon,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 60),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(snapshot.data!.about,
-                          style: TextStyle(
-                            fontSize: 16,
-                          )),
-                      const SizedBox(
-                        height: 15,
-                      ),
-                      Text('${snapshot.data!.genre} / ${snapshot.data!.age}',
-                          style: TextStyle(
-                            fontSize: 16,
-                          )),
-                    ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 50,
+            vertical: 30,
+          ),
+          child: Column(children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Hero(
+                  tag: widget.id,
+                  child: Container(
+                    width: 200,
+                    clipBehavior: Clip.hardEdge,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: 15,
+                            offset: const Offset(10, 10),
+                            color: Colors.black.withOpacity(0.5),
+                          )
+                        ]),
+                    child: Image.network(widget.thumb),
                   ),
-                );
-              }
-              return Text("...");
-            })
-      ]),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            FutureBuilder(
+                future: webtoon,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(snapshot.data!.about,
+                            style: const TextStyle(
+                              fontSize: 13,
+                            )),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text('${snapshot.data!.genre} / ${snapshot.data!.age}',
+                            style: const TextStyle(
+                              fontSize: 13,
+                            )),
+                      ],
+                    );
+                  }
+                  return Text("...");
+                }),
+            const SizedBox(height: 20),
+            FutureBuilder(
+              future: episodes,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Column(
+                    children: [
+                      for (var epsiode in snapshot.data!)
+                        Container(
+                          margin: EdgeInsets.only(bottom: 10),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.green.shade400,
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 10,
+                              horizontal: 20,
+                            ),
+                            child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    epsiode.title,
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  const Icon(
+                                    Icons.chevron_right_rounded,
+                                    color: Colors.white,
+                                  ),
+                                ]),
+                          ),
+                        )
+                    ],
+                  );
+                }
+                return Container();
+              },
+            )
+          ]),
+        ),
+      ),
     );
   }
 }
